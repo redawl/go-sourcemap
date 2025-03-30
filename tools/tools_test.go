adb8a7db09e951502452c1e939ea752034f54ddd
Eli Burch <eli.burch@burchbytes.com>
2025-03-29 20:41:39 -0700

    Add tests for tools

---
+++ tools/tools_test.go
@@ -0,0 +1,38 @@
+package tools
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var testFiles = []string{
+    "test1.js.map", 
+    "test2.js.map",
+}
+
+func TestParseSourceMapFromUrl(t *testing.T) {
+    server := httptest.NewServer(http.FileServer(http.Dir("../testdata")))
+
+    for _, testFile := range testFiles {
+        t.Run(testFile, func(t *testing.T) {
+            _, err := ParseSourceMapFromUrl(server.URL + "/" + testFile)
+
+            if err != nil {
+                t.Errorf("Error parsing %s: %v", testFile, err)
+            }
+        })
+    }
+}
+
+func TestParseSourceMapFromFile(t *testing.T) {
+    for _, testFile := range testFiles {
+        t.Run(testFile, func(t *testing.T) {
+            _, err := ParseSourceMapFromFile("../testdata/" + testFile)
+
+            if err != nil {
+                t.Errorf("Error parsing %s: %v", testFile, err)
+            }
+        })
+    }
+}
