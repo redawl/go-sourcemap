f97d0a2d5ca70ea8ef9dd49d4633f6ba6bc9415a
Pedro Tanaka <pedro.tanaka@shopify.com>
2025-09-18 11:57:22 +0200

    feat: Add comprehensive benchmarks for sourcemap parsing
    Add benchmarks for ParseSourceMap() and related functions with real-world
    sourcemaps ranging from 348 bytes to 18MB. Includes performance tests for:
    - ParseSourceMap() - full parsing pipeline
    - ParseJSON() - JSON parsing only
    - DecodeSourceMap() - decoding without JSON parsing
    - DecodeMappings() - VLQ base64 decoding
    
    Test data includes production sourcemaps from jQuery (135KB), Angular Core
    (2.7MB), and Babylon.js (18MB) to ensure parser handles real-world cases
    efficiently. Results show good scalability with ~128 MB/s throughput on
    large files.
    
    Signed-off-by: Pedro Tanaka <pedro.tanaka@shopify.com>

---
+++ spec/parse_bench_test.go
@@ -0,0 +1,105 @@
+package spec
+
+import (
+	"os"
+	"testing"
+)
+
+var benchmarkFiles = map[string]string{
+	"Tiny":   "test1.js.map",      // 348 bytes
+	"Small":  "test2.js.map",      // 459 bytes
+	"Medium": "jquery.min.map",    // 135 KB
+	"Large":  "angular-core.mjs.map", // 2.7 MB
+	"XLarge": "babylon.js.map",    // 18 MB
+}
+
+func loadBenchmarkFile(filename string) string {
+	contents, err := os.ReadFile("../testdata/" + filename)
+	if err != nil {
+		panic(err)
+	}
+	return string(contents)
+}
+
+func BenchmarkParseSourceMap(b *testing.B) {
+	for name, filename := range benchmarkFiles {
+		contents := loadBenchmarkFile(filename)
+		
+		b.Run(name, func(b *testing.B) {
+			b.SetBytes(int64(len(contents)))
+			b.ResetTimer()
+			
+			for i := 0; i < b.N; i++ {
+				_, err := ParseSourceMap(contents, "")
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkParseJSON(b *testing.B) {
+	for name, filename := range benchmarkFiles {
+		contents := loadBenchmarkFile(filename)
+		
+		b.Run(name, func(b *testing.B) {
+			b.SetBytes(int64(len(contents)))
+			b.ResetTimer()
+			
+			for i := 0; i < b.N; i++ {
+				_, err := ParseJSON(contents)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkDecodeSourceMap(b *testing.B) {
+	for name, filename := range benchmarkFiles {
+		contents := loadBenchmarkFile(filename)
+		sourceMap, err := ParseJSON(contents)
+		if err != nil {
+			b.Fatal(err)
+		}
+		
+		b.Run(name, func(b *testing.B) {
+			b.ResetTimer()
+			
+			for i := 0; i < b.N; i++ {
+				_, err := DecodeSourceMap(sourceMap, "")
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkDecodeMappings(b *testing.B) {
+	for name, filename := range benchmarkFiles {
+		contents := loadBenchmarkFile(filename)
+		sourceMap, err := ParseJSON(contents)
+		if err != nil {
+			b.Fatal(err)
+		}
+		
+		sources, err := DecodeSourceMapSources("", sourceMap.SourceRoot, sourceMap.Sources, sourceMap.SourcesContent, sourceMap.IgnoreList)
+		if err != nil {
+			b.Fatal(err)
+		}
+		
+		b.Run(name, func(b *testing.B) {
+			b.ResetTimer()
+			
+			for i := 0; i < b.N; i++ {
+				_, err := DecodeMappings(sourceMap.Mappings, sourceMap.Names, sources)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
